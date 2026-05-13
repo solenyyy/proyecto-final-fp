81bc3bece4d435fb361eff07b5e95c3f99303a4e
Soleny <solenyyumare@gmail.com>
2026-05-13 19:51:03 +0100

    back

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -12,3 +12,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
+
+
