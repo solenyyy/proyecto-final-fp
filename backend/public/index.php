4bca3ada20ef3c7a5060d517018d1088fdb722a1
Soleny <solenyyumare@gmail.com>
2026-05-13 18:35:38 +0100

    555

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,24 +2,7 @@
 
 use App\Kernel;
 
-if (!headers_sent()) {
-    header('Access-Control-Allow-Origin: https://proyecto-final-fp-five.vercel.app');
-    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-}
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header('HTTP/1.1 200 OK');
-    exit();
-}
-
-$autoload = dirname(__DIR__).'/vendor/autoload_runtime.php';
-if (!file_exists($autoload)) {
-    $autoload = dirname(__DIR__).'/vendor/autoload.php';
-}
-
-require_once $autoload;
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
