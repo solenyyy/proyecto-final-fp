bf562766ec358e6a5f108680aaf7eb478fc7082a
Soleny <solenyyumare@gmail.com>
2026-05-13 18:05:17 +0100

    222

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,6 +2,16 @@
 
 use App\Kernel;
 
+header('Access-Control-Allow-Origin: https://proyecto-final-fp-five.vercel.app');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
