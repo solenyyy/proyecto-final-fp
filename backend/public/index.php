1cd419802ce0fb0207244bb806c87af69fdffcde
Soleny <solenyyumare@gmail.com>
2026-05-13 18:17:57 +0100

    333

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,7 +2,6 @@
 
 use App\Kernel;
 
-// 1. CABECERAS CORS (Indispensable para Vercel)
 header('Access-Control-Allow-Origin: https://proyecto-final-fp-five.vercel.app');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
@@ -13,13 +12,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit();
 }
 
-// 2. CARGA DINÁMICA DEL AUTOLOAD
-// Probamos la ruta estándar de Symfony
 $autoload = dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 if (!file_exists($autoload)) {
-    // Si falla, probamos la ruta absoluta del contenedor de Railway
-    $autoload = '/app/vendor/autoload_runtime.php';
+    $autoload = dirname(__DIR__).'/vendor/autoload.php';
 }
 
 require_once $autoload;
